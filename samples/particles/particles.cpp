855e7b5a39d4e0a2bf8258999519678832481259
fsole <wfsole@gmail.com>
2018-05-28 19:01:31 +0200

    Updated particles image

--- samples/particles/particles.cpp
+++ samples/particles/particles.cpp
@@ -471,7 +471,7 @@ public:
     render::context_t& context = getRenderContext();
 
     VkClearValue clearValues[2];
-    clearValues[0].color = { { 0.2f, 0.3f, 0.4f, 1.0f } };
+    clearValues[0].color = { { 0.0f, 0.0f, 0.0f, 1.0f } };
 
     clearValues[1].depthStencil = { 1.0f,0 };
     const VkCommandBuffer* commandBuffers;
